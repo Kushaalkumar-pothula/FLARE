2416ed5719f20b268fc8673ab3cc9d40f94c2536
Kushaal Kumar Pothula <66947837+Kushaalkumar-pothula@users.noreply.github.com>
2021-02-21 14:04:13 +0530

    Update fluence_analysis.py

--- fluence_analysis.py
+++ fluence_analysis.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 fig = plt.gcf()
 fig.set_size_inches(8,6)
-plt.hist(np.log10(fluence), bins = 500, alpha = 0.5, color = 'orange')
+plt.hist(radio_fluence, bins = 500, alpha = 0.5, color = 'orange')
 plt.xlabel(r"$Radio\ Fluence\ [\frac{erg}{cm^2}]$", fontsize = 15,)
 plt.ylabel("Number of FRBs",fontsize = 15)
 plt.grid(True)
