c75f09106beb424dfaffeaa540ad9f6b7890eef7
Kushaal Kumar Pothula <66947837+Kushaalkumar-pothula@users.noreply.github.com>
2021-03-01 18:23:13 +0530

    Update fluence.py

--- fluence.py
+++ fluence.py
@@ -2,13 +2,13 @@ import numpy as np
 import os
 
 #------------------------------File IO (input)------------------------------
-i_1 = -2
+i_1 = -3
 while os.path.exists("flare_distance_result%s.txt" % i_1):
     i_1 += 1
 
 np.loadtxt("flare_distance_result%s.txt" % i_1)
 
-i_2 = -2
+i_2 = -3
 while os.path.exists("flare_energy_result%s.txt" % i_2):
     i_2 += 1
 
