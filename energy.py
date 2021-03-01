2a2a9acfe27d383fcedd1d266ea8a6387c8ca0c4
Kushaal Kumar Pothula <66947837+Kushaalkumar-pothula@users.noreply.github.com>
2021-03-01 18:29:03 +0530

    Update energy.py

--- energy.py
+++ energy.py
@@ -26,7 +26,7 @@ for i in range(100000):
 #----------------------------------File IO---------------------------------
 arr_e = np.array(energy)
 
-i = -2
+i = -1
 while os.path.exists("flare_energy_result%s.txt" % i):
     i += 1
 
