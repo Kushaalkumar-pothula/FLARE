995e6c45bd758546494783163166cc63c6a9c8bc
Kushaal Kumar Pothula <66947837+Kushaalkumar-pothula@users.noreply.github.com>
2021-03-07 15:12:54 +0530

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -7,7 +7,7 @@ echo "|  _| | |    |  _  ||    /|  __| "
 echo "| |   | |____| | | || |\ \| |___ "
 echo "\_|   \_____/\_| |_/\_| \_\____/ "
 
-echo "Welcome to FLARE v2.0, now including optimized parallel processing and data loading. "
+echo "Welcome to FLARE v2.0, now including optimized parallel processing and data loading mechanism. "
                                  
                                  
 current_date_time="`date "+%Y-%m-%d %H:%M"`";
